3b28adf7084899f79f5351c055ade581da437ff6
Eva Pace <eba.pachi@gmail.com>
2024-03-20 00:04:19 -0300

    mock implementation using BTreeMap

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,35 @@ pub trait KV {
     fn put(&mut self, key: &[u8], value: &[u8]) -> Result<(), Error>;
     fn delete(&mut self, key: &[u8]) -> Result<(), Error>;
 }
+
+use std::collections::BTreeMap;
+
+pub struct Database {
+    map: BTreeMap<Vec<u8>, Vec<u8>>,
+}
+
+impl Database {
+    pub fn new() -> Self {
+        Self { map: BTreeMap::new() }
+    }
+}
+
+impl KV for Database {
+    fn get(&self, key: &[u8]) -> Result<Vec<u8>, Error> {
+        match self.map.get(key) {
+            Some(value) => Ok(value.to_vec()),
+            None => Err(Error::KeyNotFound),
+        }
+    }
+    fn has(&self, key: &[u8]) -> Result<bool, Error> {
+        Ok(self.map.contains_key(key))
+    }
+    fn put(&mut self, key: &[u8], value: &[u8]) -> Result<(), Error> {
+        self.map.insert(key.to_vec(), value.to_vec());
+        Ok(())
+    }
+    fn delete(&mut self, key: &[u8]) -> Result<(), Error> {
+        self.map.remove(key);
+        Ok(())
+    }
+}
